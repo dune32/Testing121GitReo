1ee0b4999fbee73dd9780543b454ac419582fbf6
dune32 <81596769+dune32@users.noreply.github.com>
2025-01-30 23:10:16 -0600

    Add files via upload

---
+++ duckbear/Program.cs
@@ -0,0 +1,11 @@
+﻿
+            Console.WriteLine("How many eggs were there today?");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int eggsForEachSister = length / 4;
+            int eggsForDuckbear = length % 4;
+
+            Console.WriteLine("Each Sister gets " + eggsForEachSister + " chocolate eggs.");
+
+            Console.WriteLine("The duckbear gets " + eggsForDuckbear + " chocolate eggs.");
+ 
