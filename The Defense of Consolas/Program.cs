4d377fd0c4f8a20faba811f379bc510fee2b902b
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-02 20:49:19 -0600

    Add files via upload

---
+++ The Defense of Consolas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+// Expected results 
+//Target row 6
+//Target Colum 5
+//Deploy to (6,4) (5,5)(6,6) and (7,5)
+// [x] ASK user for the target row and column.
+// [x] Compute the neighboring rows and colums of where to deply the squad.
+// [] Display the deployment instructions in a different color of your choosing.
+// [x] Change the window title to be "Defense of Consolas"
+// [] Play a sound with Console.Beep when the results have been computed and displayed.
+
+
+{
+    Console.Title = "Defense of Consolas";
+
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.Write("What row is your airship on? ");
+    int row = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("What is your target column? ");
+    int column = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Deploy to:");
+
+    Console.WriteLine("(" + row + ", " + (column -1) + ")");
+    Console.WriteLine("(" + (row - 1) + ", " + column + ")");
+    Console.WriteLine("(" + row + ", " + (column + 1)+ ")");
+    Console.WriteLine("(" + (row + 1) + ", " + column + ")");
+
+    Console.Beep();
+    Console.ReadKey();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
