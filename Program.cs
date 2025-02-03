23baf1c5ae7f3d5641f7fe2e4459f58b34d305e9
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-02 18:40:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// Without using system we cannot call the mainframes basic functions.
+
+
+{
+
+
+    {
+        Console.WriteLine("How many estates do you have?");
+        int estateCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("How many duchies do you have?");
+        int duchyCount = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("How many provinces do you have?");
+        int provinceCount = Convert.ToInt32(Console.ReadLine());
+
+
+        int estatePointValue   = 1 * estateCount;
+        int duchyPointValue    = 3 * duchyCount;
+        int provincePointValue = 6 * provinceCount;
+
+        int total = estatePointValue + provincePointValue + duchyPointValue;
+
+        Console.WriteLine("You have a total of " + total + " points.");
+
+    }
+}
+
+
+ 
