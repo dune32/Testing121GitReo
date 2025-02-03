6ae02d9fff80c8c78de461efbd9c45b36949f8a6
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-02 21:29:41 -0600

    Add files via upload

---
+++ Repairing the Clocktower/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Repairing_the_Clocktower
+
+{
+    class Program
+    {
+        static void Main() // Without this if statements and remainder function don't work?
+        {
+            // [x] Take a number as input from the console
+            // [x] Display the word "Tick" if the number is even. Display the word "Tock" if the number is odd/
+
+
+            Console.WriteLine("Enter the number: ");
+
+
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            int remainder = number % 2;
+
+
+            if (remainder == 0)
+                Console.WriteLine("Tick");
+            else Console.WriteLine("Tock");
+
+
+
+
+
+
+
+        }
+    }
+}
