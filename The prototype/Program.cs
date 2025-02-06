3b6b064aa6b94a84f294863973a7d16feba771a6
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-06 11:25:26 -0600

    Add files via upload

---
+++ The prototype/Program.cs
@@ -0,0 +1,32 @@
+﻿// [x] if the number is above 100 or less than 0 keep asking.
+// [x] clear the screen once the program has collected a good number.
+// [x] ask a second user, the hunter, to guess numbers.
+// [x] indicate whether the user guessed too high or to low or guessed right.
+// [x] Loop until they get it right, then end the program.
+
+
+int number;
+do
+{
+    Console.Write("User 1,Enter a number between 0 and 100: ");
+   number = Convert.ToInt32(Console.ReadLine());
+} while (number < 0 || number > 100);
+
+Console.Clear();
+
+Console.WriteLine("User 2, guess the number");
+Console.WriteLine("What is your next guess? ");
+
+int guess;
+
+do
+{
+
+    guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess == number) Console.WriteLine("You guessed right!");
+    else if (guess > number) Console.WriteLine($"{guess} too high!");
+    else Console.WriteLine($"{guess} is to low!");
+
+} while (guess != number);
+
---
+++ The replicator of d'to/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace The_replicator_of_d_to
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[5];
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write("Enter number: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+                number[i] = value;
+            }
+            int [] copy = new int[5];
+            
+           for (int i = 0; i < number.Length;i++)
+           copy[i] = number[i];
+
+           for (int i = 0; i < number.Length; i++)
+           Console.WriteLine(number[i]);
+            
+           for (int i = 0; i < copy.Length; i++)
+           Console.WriteLine(copy[i]);
+            }
+        }
+    }
+
