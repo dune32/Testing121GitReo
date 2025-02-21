00f6da3633398e8b8b2017dbf57a6c8514e95aff
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-20 23:04:17 -0600

    Add files via upload
    question 4 of homework 4 -

---
+++ question 4 on homework 4/question 4 on homework 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_4_on_homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
