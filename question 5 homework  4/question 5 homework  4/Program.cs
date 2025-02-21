5170dc40ee4fdddc3d355d318ca8beca649e7e8c
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-20 23:20:05 -0600

    Add files via upload

---
+++ question 5 homework  4/question 5 homework  4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ChangeAll
+{
+    public static string Move(string s)
+    {
+        string change = "";
+
+        foreach (char c in s)
+        {
+            // Shift each letter by 1
+            change += (char)(c + 1);
+        }
+
+        return change;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Move("hello"));    // ➞ "ifmmp"
+        Console.WriteLine(Move("bye"));      // ➞ "czf"
+        Console.WriteLine(Move("welcome"));  // ➞ "xfmdpnf"
+    }
+}
