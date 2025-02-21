a0d19b562cd55ec15b390b29194e39076790b395
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-20 23:36:43 -0600

    Add files via upload

---
+++ Order question 3  homework 4/Order question 3  homework 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class InOrder
+{
+    public static bool IsInOrder(string str)
+    {
+
+        for (int i = 0; i < str.Length - 1; i++)
+        {
+            if (str[i] > str[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void Main()
+    {
+
+        Console.WriteLine(IsInOrder("abc"));    // Output: true
+        Console.WriteLine(IsInOrder("edabit")); // Output: false
+        Console.WriteLine(IsInOrder("123"));    // Output: true
+        Console.WriteLine(IsInOrder("xyzz"));   // Output: true
+    }
+}
