cb9fe70c659250765f01105fe95d0ef535e41f2f
dune32 <81596769+dune32@users.noreply.github.com>
2025-02-27 23:56:34 -0600

    Add files via upload

---
+++ HW problem 1/Motorcycle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_problem_1
+{
+    public class Motorcycle : Vehicle
+    {
+
+        //var for sidecare
+        private bool hasSideCar = false;
+
+
+        //Get and set for sidecar
+
+        public bool HasSideCar
+        {
+            get { return hasSideCar; }
+            set { hasSideCar = value; }
+        }
+
+        public Motorcycle(bool ahasSideCar, string amake, string amodel, int ayear) // the motorcycles new constructor 
+            : base(amake, amodel, ayear) // Calling the base class constructor
+        {
+            this.hasSideCar = ahasSideCar;
+            this.Make = amake;
+            this.Model = amodel;
+            this.Year = ayear;
+        }
+
+
+        public override string ToString() // override to string
+        {
+            string message = "";
+            message += $"{Year} {Make} {Model} (Has Sidecar: {HasSideCar}).";
+            return message;
+        }
+
+
+
+
+
+    }
+}
---
+++ HW problem 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_problem_1
+{ // Vehicle class} 
+    public class Vehicle //base class
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public Vehicle() { } // empty constructor
+        public Vehicle(string make, string model, int year) // full constructor
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public override string ToString()
+        {
+            return $"{Year} {Make} {Model}";
+        }
+
+
+        static void Main(string[] args)
+
+        { }
+    }
+}
+        
+        
+    
+       
